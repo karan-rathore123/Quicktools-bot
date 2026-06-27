de982a68c602c041709a5d4b15fa18b8ae3f86ad
Karan Harsh Singh <ask.geminiservices@gmail.com>
2026-06-27 08:42:59 +0530

    debugging

--- services/ilovepdf.js
+++ services/ilovepdf.js
@@ -2,6 +2,14 @@ require("dotenv").config();
 
 const ILovePDFApi = require("@ilovepdf/ilovepdf-nodejs");
 
+console.log("===== iLovePDF ENV CHECK =====");
+console.log("PUBLIC_KEY =", process.env.PUBLIC_KEY);
+console.log(
+  "SECRET_KEY length =",
+  process.env.SECRET_KEY?.length
+);
+console.log("==============================");
+
 const instance = new ILovePDFApi(
   process.env.PUBLIC_KEY,
   process.env.SECRET_KEY,
